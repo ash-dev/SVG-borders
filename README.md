6b04ac218b8e8a50efda4c6b3dd3f3e9d7ad8fb3
ash-dev <ash.dev.github@gmail.com>
2017-08-26 14:07:03 -0700

    Upload 1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# SVG-borders
+
+Easy SVG borders for whimsical section separation. To implement, download the relevant SVG package.
+
+## Demo
+
+https://ash-dev.github.io/SVG-borders
+
+## Preview
+
+![Color schemes](https://github.com/ash-dev/SVG-borders/blob/master/images/demo.jpg?raw=true)
---
+++ svg-borders.php
@@ -0,0 +1,97 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SVG Borders</title>
+  <meta name="description" content="Easy SVG borders for whimsical section separation. To implement, download the relevant SVG package.">
+  <!---Normalize--->  
+  <link rel="stylesheet" href="css/normalize.css">
+  <!---Demo resources--->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+  <script src="https://use.fontawesome.com/74cc847491.js"></script>
+  <link rel="stylesheet" href="css/styles.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<h1>SVG Borders</h1>
+<p class="description">Easy SVG borders for whimsical section separation. To implement, download the relevant SVG package.</p>
+
+<!---Torn paper--->
+
+<section id="torn-paper">
+<?php include 'partial/torn-paper.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/torn-paper.html" class="button" download="">Download Torn Paper SVG Border <i class="fa fa-download"></i></a></button>	
+</div>
+
+<!---Chevron--->
+
+<section id="chevron">
+<?php include 'partial/chevron.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/chevron.html" class="button" download="">Download Chevron SVG Border <i class="fa fa-download"></i></a></button>		
+</div>
+
+<!---Dashed--->
+
+<section id="dashed">
+<?php include 'partial/dashed.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/dashed.html" class="button" download="">Download Dashed SVG Border <i class="fa fa-download"></i></a></button>	
+</div>
+
+<!---Dotted--->
+
+<section id="dotted">
+<?php include 'partial/dotted.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/dotted.html" class="button" download="">Download Dotted SVG Border <i class="fa fa-download"></i></a></button>	
+</div>
+
+<!---Checkered--->
+
+<section id="checkered">
+<?php include 'partial/checkered.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/checkered.html" class="button" download="">Download Checkered SVG Border <i class="fa fa-download"></i></a></button>
+</div>
+
+<!---Wavy--->
+
+<section id="wavy">
+<?php include 'partial/wavy.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/wavy.html" class="button" download="">Download Wavy SVG Border <i class="fa fa-download"></i></a></button>
+</div>
+
+<!---Scalloped--->
+
+<section id="scalloped">
+<?php include 'partial/scalloped.html'; ?>
+</section>
+
+<div class="downloads">
+<button><a href="partial/scalloped.html" class="button" download="">Download Scalloped SVG Border <i class="fa fa-download"></i></a></button>
+</div>
+
+</body>
+</html>
